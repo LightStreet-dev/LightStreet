972411e4751f8e70e3717cb8836974f861d486d2
LightStreet <light.street.web@gmail.com>
2025-07-25 22:19:53 +0200

    added target

--- src/components/SocialIcons/SocialIcons.tsx
+++ src/components/SocialIcons/SocialIcons.tsx
@@ -7,14 +7,14 @@ interface socialProps {
 const SocialIcons: React.FC<socialProps> = ({ isOpen }) => {
   return (
     <ul className={clsx(s.socIconsList, isOpen && s.socIconsListModal)}>
-      <li><a href="https://www.instagram.com/dimonchikseagull/">
+      <li><a href="https://www.instagram.com/dimonchikseagull/" target="blank">
         <svg className ={clsx(s.svg, isOpen && s.svgModal)} >
           <use href="/svg/symbol-defs.svg#icon-instagramLS"></use>
         </svg>
         </a>
       </li>
       <li>
-        <a href="https://www.linkedin.com/in/seagull-dmytro/">
+        <a href="https://www.linkedin.com/in/seagull-dmytro/" target="blank">
         <svg className ={clsx(s.svg, isOpen && s.svgModal)} >
           <use href="/svg/symbol-defs.svg#icon-linkedin-LS"></use>
         </svg>
