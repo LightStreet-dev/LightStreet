6a762837275b042477749c9236aa43d330aecb2f
LightStreet <light.street.web@gmail.com>
2025-07-25 22:17:48 +0200

    added my soc

--- src/components/SocialIcons/SocialIcons.tsx
+++ src/components/SocialIcons/SocialIcons.tsx
@@ -7,14 +7,14 @@ interface socialProps {
 const SocialIcons: React.FC<socialProps> = ({ isOpen }) => {
   return (
     <ul className={clsx(s.socIconsList, isOpen && s.socIconsListModal)}>
-      <li><a href="#">
+      <li><a href="https://www.instagram.com/dimonchikseagull/">
         <svg className ={clsx(s.svg, isOpen && s.svgModal)} >
           <use href="/svg/symbol-defs.svg#icon-instagramLS"></use>
         </svg>
         </a>
       </li>
       <li>
-        <a href="#">
+        <a href="https://www.linkedin.com/in/seagull-dmytro/">
         <svg className ={clsx(s.svg, isOpen && s.svgModal)} >
           <use href="/svg/symbol-defs.svg#icon-linkedin-LS"></use>
         </svg>
