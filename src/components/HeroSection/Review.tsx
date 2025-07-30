9feb84127e024178d83626a661654ac40ff74cd4
LightStreet <light.street.web@gmail.com>
2025-07-30 22:06:46 +0200

    mobile revwievs done

--- src/components/HeroSection/Review.tsx
+++ src/components/HeroSection/Review.tsx
@@ -1,18 +1,30 @@
-import s from './Review.module.css'
-import type { Review as ReviewType } from '../../data/reviews';
+import s from "./Review.module.css";
+import type { Review as ReviewType } from "../../data/reviews";
+import clsx from "clsx";
+
 interface ReviewProps {
   review: ReviewType;
+  index: number | null;
+  activeIndex: number | null;
+  setActiveIndex: React.Dispatch<React.SetStateAction<number | null>>;
 }
 
-
-const Review: React.FC<ReviewProps> = ({ review }) => {
-    const { photo, text } = review;
+const Review: React.FC<ReviewProps> = ({
+  review,
+  index,
+  activeIndex,
+  setActiveIndex,
+}) => {
+  const { photo } = review;
+const isActive = activeIndex === index
+  const handleClickRewiev = () => {
+    setActiveIndex(index);
+  };
   return (
-    <div className={s.reviewWraper}>
-        <img className={s.reviewImg} src={photo} alt="Review" />
-        <p className={`hidden ${s.reviesText}` }>{text}</p>
+    <div className={s.reviewWraper} onClick={handleClickRewiev}>
+      <img className={clsx(s.reviewImg, isActive && s.activeImg)} src={photo} alt="Review" />
     </div>
   );
 };
 
-export default Review
+export default Review;
--- src/components/HeroSection/ReviewsContainer.tsx
+++ src/components/HeroSection/ReviewsContainer.tsx
@@ -1,15 +1,32 @@
 import s from "./ReviewsContainer.module.css";
 import reviews from "../../data/reviews.ts";
 import Review from "./Review.tsx";
-
+import { useState } from "react";
 
 const ReviewsContainer: React.FC = () => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const activeReview = activeIndex !== null ? reviews[activeIndex] : null;
   return (
-    <div className={s.reviewsContainer}>
-      {reviews.map((review) => {
-        return  <Review review={review} key={review.id} />;
-      })}
-    </div>
+    <>
+      <div className={s.reviewsContainer}>
+        {reviews.map((review, index) => {
+          return (
+            <Review
+              review={review}
+              key={review.id}
+              index={index}
+              activeIndex={activeIndex}
+              setActiveIndex={setActiveIndex}
+            />
+          );
+        })}
+      </div>
+      {activeReview && (
+        <p className={s.reviewText}>
+          {activeReview.text}
+        </p>
+      )}
+    </>
   );
 };
 
