19eda0e8f33c1f28566bca11f210f922c2a90ce7
LightStreet <light.street.web@gmail.com>
2025-08-05 23:03:35 +0200

    test reviews

--- src/components/HeroSection/ReviewsContainer.tsx
+++ src/components/HeroSection/ReviewsContainer.tsx
@@ -2,6 +2,7 @@ import s from "./ReviewsContainer.module.css";
 import reviews from "../../data/reviews.ts";
 import Review from "./Review.tsx";
 import { useState } from "react";
+import clsx from "clsx";
 
 const ReviewsContainer: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState<number | null>(() => Math.floor(Math.random() * reviews.length));
@@ -9,6 +10,7 @@ const ReviewsContainer: React.FC = () => {
   const activeReview = activeIndex !== null ? reviews[activeIndex] : null;
   return (
     <div className={s.rewiewsWrapper}>
+      <div className={s.wrapWithText}>
       <div className={s.reviewsContainer}>
         {reviews.map((review, index) => {
           return (
@@ -21,6 +23,9 @@ const ReviewsContainer: React.FC = () => {
             />
           );
         })}
+        
+      </div>
+      <p className={clsx(s.reviewsText, activeIndex && s.reviewsTextActive)}>Our <br />reviews</p>
       </div>
       {activeReview && (
         <p className={s.reviewText}>
