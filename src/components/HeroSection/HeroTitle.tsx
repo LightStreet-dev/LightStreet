d0bd8cc694cc0d572cdda93fb1b4df7c425d644a
LightStreet <light.street.web@gmail.com>
2025-08-05 21:31:36 +0200

    clsx hero title

--- src/components/HeroSection/HeroTitle.tsx
+++ src/components/HeroSection/HeroTitle.tsx
@@ -1,10 +1,15 @@
+
+import clsx from "clsx";
+import i18n from "../../i18n";
 import s from "./HeroTitle.module.css";
 import { useTranslation } from 'react-i18next'
 const HeroTitle:React.FC = () => {
   const {t} = useTranslation();
+ const isPL = i18n.language === 'pl';
+
     return (
     <div className={s.heroTitleContainer}>
-      <h1 className={s.heroTitle}>
+      <h1 className={clsx(s.heroTitle, isPL && s.heroTitlePl)}>
         <span className={s.heroTitleContent1}>{t("hero.heroTitle1")}</span>
         <span className={s.heroTitleContent2}>{t("hero.heroTitle2")}</span>
         <span className={s.heroTitleContent3}>{t("hero.heroTitle3")}</span>
