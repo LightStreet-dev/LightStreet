613514b301e35dd59d4a7c8ba4414cc4a7ab81d8
LightStreet <light.street.web@gmail.com>
2025-09-18 20:38:59 +0200

    navigation done

--- src/components/OferSectionComponent/OfersListComponent/SwiperNavigationComponent.tsx
+++ src/components/OferSectionComponent/OfersListComponent/SwiperNavigationComponent.tsx
@@ -1,15 +1,51 @@
 import { useSwiper } from "swiper/react";
 import s from "./SwiperNavigationComponent.module.css";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
+
 const SwiperNavigationComponent = () => {
   const swiper = useSwiper();
+  const [isBeginning, setIsBeginning] = useState(true);
+  const [isEnd, setIsEnd] = useState(false);
+
+  useEffect(() => {
+    if (!swiper) return;
+
+    // виставляємо початковий стан
+    setIsBeginning(swiper.isBeginning);
+    setIsEnd(swiper.isEnd);
+
+    // слухач оновлення
+    const handleUpdate = () => {
+      setIsBeginning(swiper.isBeginning);
+      setIsEnd(swiper.isEnd);
+    };
+
+    swiper.on("slideChange", handleUpdate);
+
+    return () => {
+      swiper.off("slideChange", handleUpdate);
+    };
+  }, [swiper]);
+
   if (!swiper) return null;
+
   return (
     <div className={s.swiperNavBtn}>
-      <button className={s.swiperBtn} onClick={() => swiper.slidePrev()}>
-      <img className={s.arrowSvg} src="/svg/arrow-left.svg"  alt="<" />
+      <button
+      className={clsx(s.swiperBtn, isBeginning && s.swiperBtnDisabled)}
+        onClick={() => swiper.slidePrev()}
+        disabled={isBeginning} // 🚫 не активна на першому слайді
+      >
+        <img className={s.arrowSvg} src="/svg/arrow-left.svg" alt="Назад" />
       </button>
-      <button className={s.swiperBtn} onClick={() => swiper.slideNext()}>
-       <img className={s.arrowSvg} src="/svg/arrow-right.svg"  alt=">" />
+
+      <button
+        className={clsx(s.swiperBtn, isEnd && s.swiperBtnDisabled)}
+        onClick={() => swiper.slideNext()}
+        disabled={isEnd} // 🚫 не активна на останньому слайді
+      >
+        <img   className={s.arrowSvg} src="/svg/arrow-right.svg" alt="Далі" />
       </button>
     </div>
   );
