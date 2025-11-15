ecd94269dbaeb427f28b9f9d48f835e6b934d1e6
LightStreet <light.street.web@gmail.com>
2025-11-15 20:54:30 +0100

    spiner changed

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,14 @@ import './index.css'
 import 'modern-normalize'
 import App from './App.tsx'
 import './i18n.js';
-import { PacmanLoader } from 'react-spinners'
+import {  PropagateLoader } from 'react-spinners'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Suspense fallback={<PacmanLoader
-    className="loader"
-  color="#d4e9e6"
+    <Suspense fallback={<PropagateLoader
+    className="loader LSloader"
+  color="hsla(204, 100%, 50%, 0.08)"
   size={30}
 />}>
     <App />
