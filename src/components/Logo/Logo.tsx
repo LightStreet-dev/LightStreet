3d0a81521cb755c9f285fd786895aa88ad102e20
LightStreet <light.street.web@gmail.com>
2025-11-25 20:18:51 +0100

    fixed logo header

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,7 +2,9 @@ import s from './Logo.module.css'
 function Logo() {
     return(
         <a className={s.logoWrapper}>
-            <img className={s.logoImg} src="/img/Logo/LSLogo.svg" alt="Logo" />
+            <svg className={s.logoImg}>
+            <use href="/svg/symbol-defs.svg#LSLogo"></use>
+          </svg>
             <h2 className={s.logo}>Light Street</h2>
         </a>
     )
