d74fe96f856be051f151109a5437c0521e713a00
LightStreet <light.street.web@gmail.com>
2025-11-27 22:11:39 +0100

    FIXED MAIL VAL FORM

--- src/validators/mainFormValidation.ts
+++ src/validators/mainFormValidation.ts
@@ -8,25 +8,25 @@ const phoneValidation = /^\+?\d{1,15}$/;
 
 const mainFormSchema = Yup.object().shape({
   Name: Yup.string()
-    .required(() => t("formTranslation:errors.error"))
-    .matches(letterValidation, () => t("formTranslation:errors.onlyLetters"))
-    .min(3, () => t("formTranslation:errors.min"))
-    .max(20, () => t("formTranslation:errors.max")),
+    .required(() => t("formTranslation.errors.error"))
+    .matches(letterValidation, () => t("formTranslation.errors.onlyLetters"))
+    .min(3, () => t("formTranslation.errors.min"))
+    .max(20, () => t("formTranslation.errors.max")),
 
   Company: Yup.string()
-    .min(3, () => t("formTranslation:errors.min"))
-    .max(20, () => t("formTranslation:errors.max")),
+    .min(3, () => t("formTranslation.errors.min"))
+    .max(20, () => t("formTranslation.errors.max")),
 
-  Phone: Yup.string().required(() => t("formTranslation:errors.error")).matches(phoneValidation, () => t("formTranslation:errors.phone")),
+  Phone: Yup.string().required(() => t("formTranslation.errors.error")).matches(phoneValidation, () => t("formTranslation.errors.phone")),
 
   Email: Yup.string()
-    .matches(mailValidation, () => t("formTranslation:errors.mail"))
+    .matches(mailValidation, () => t("formTranslation.errors.mail"))
 
-    .required(() => t("formTranslation:errors.error"))
-    .min(3, () => t("formTranslation:errors.min"))
-    .max(30, () => t("formTranslation:errors.maxText")),
+    .required(() => t("formTranslation.errors.error"))
+    .min(3, () => t("formTranslation.errors.min"))
+    .max(30, () => t("formTranslation.errors.maxText")),
 
-  Text: Yup.string().max(200, () => t("formTranslation:errors.maxText")),
+  Text: Yup.string().max(200, () => t("formTranslation.errors.maxText")),
 
   checkbox: Yup.boolean(),
 });
