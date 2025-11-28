0795c0b5c42b507fd8e297ddf42d91e83edc90fb
LightStreet <light.street.web@gmail.com>
2025-11-28 20:22:02 +0100

    decoration done

--- src/components/ContactSectionComponent/ContactSectionComponent.tsx
+++ src/components/ContactSectionComponent/ContactSectionComponent.tsx
@@ -1,21 +1,24 @@
-import s from "./ContactSectionComponent.module.css"
-import ShortFormComponent from "./ShortFormComponent"
+import s from "./ContactSectionComponent.module.css";
+import ShortFormComponent from "./ShortFormComponent";
 import { useTranslation } from "react-i18next";
 
 const ContactSectionComponent = () => {
   const { t } = useTranslation();
   return (
     <div className={s.contactWrapper}>
-<div className="container">
-    <h2 className={s.contactTitle}>{t("ContactSection.title")}</h2>
-    <p className={s.contactText}>{t("ContactSection.text")}</p>
-</div>
-<div className="container">
-<ShortFormComponent/>
-</div>
-
+      <div className="container">
+        <h2 className={s.contactTitle}>{t("ContactSection.title")}</h2>
+        <p className={s.contactText}>{t("ContactSection.text")}</p>
+      </div>
+      <svg className={s.contactLogo}>
+        <use href="/svg/symbol-defs.svg#LSLogo"></use>
+      </svg>
+      <img className={s.contactGears} src="/svg/gears.svg" alt="gears" />
+      <div className="container">
+        <ShortFormComponent />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactSectionComponent
+export default ContactSectionComponent;
