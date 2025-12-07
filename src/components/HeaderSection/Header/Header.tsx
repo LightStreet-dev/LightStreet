b70d8c37e94bf7e91b45c57f33b146d01edd9ee7
LightStreet <light.street.web@gmail.com>
2025-12-07 10:58:04 +0100

    gsap header

--- src/components/HeaderSection/Header/Header.tsx
+++ src/components/HeaderSection/Header/Header.tsx
@@ -4,38 +4,64 @@ import NavLinks from "../NavLins/NavLinks";
 import SocialIcons from "../../SocialIcons/SocialIcons";
 import s from "./Header.module.css";
 import LanguageChanger from "../LanguageChanger/LanguageChanger";
-
 import ModalMenu from "../ModalMenu/ModalMenu";
+
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+
 interface HeaderProps {
   mobMediaQuery: boolean;
   toggleForm:  (setter: React.Dispatch<React.SetStateAction<boolean>>) => void;
   setOpenForm:React.Dispatch<React.SetStateAction<boolean>>;
 }
 const Header: React.FC<HeaderProps> = ({ mobMediaQuery, toggleForm, setOpenForm }) => {
+  
+ const headerContainer = useRef<HTMLDivElement | null>(null);
+const headerContent = useRef<HTMLDivElement | null>(null);
+ useGSAP(
+    () => {
+ if (!headerContainer.current || !headerContent.current) return;
+
+  const tl = gsap.timeline({ ease: "power3.out" });
+
+      tl.from(headerContainer.current, {
+    y: -150,
+    opacity: 0,
+    duration: 1,
+  });
+   tl.from(headerContent.current, {
+    opacity: 0,
+    x: -20,
+    duration: 0.6,
+  }, "-=0.3"); 
+});
+
   return (
-    <header className={s.header}>
-   <div className={s.headerWrapper} >
-      <Logo />
+    <header className={s.header} ref={headerContainer}>
+      <div className={s.headerWrapper} ref={headerContent}>
+        <Logo />
 
-      {mobMediaQuery ? (
-        <div className={s.modalWraper}>
-          <LanguageChanger />
-          <ModalMenu toggleForm={toggleForm} setOpenForm={setOpenForm} />
-        </div>
-      ) : (
-        <>
-          <NavLinks />
-          <div className={s.mainHeaderWrapper}>
-            <div className={s.socialContactWrapper}>
-              <SocialIcons />
-            </div>
-
-            <>
-              <LanguageChanger /> <ContactButton toggleForm={toggleForm} setOpenForm={setOpenForm} />
-            </>
+        {mobMediaQuery ? (
+          <div className={s.modalWraper}>
+            <LanguageChanger />
+            <ModalMenu toggleForm={toggleForm} setOpenForm={setOpenForm} />
           </div>
-        </>
-      )}
+        ) : (
+          <>
+            <NavLinks />
+            <div className={s.mainHeaderWrapper}>
+              <div className={s.socialContactWrapper}>
+                <SocialIcons />
+              </div>
+
+              <div className={s.groupWrapper}>
+                <LanguageChanger /> <ContactButton toggleForm={toggleForm} setOpenForm={setOpenForm} />
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </header>
   );
