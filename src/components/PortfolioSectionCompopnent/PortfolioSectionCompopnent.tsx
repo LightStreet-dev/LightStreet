9021be24c0c0ddab1b2d1046969f96130105b810
LightStreet <light.street.web@gmail.com>
2025-12-13 21:47:28 +0100

    delete swiper ref

--- src/components/PortfolioSectionCompopnent/PortfolioSectionCompopnent.tsx
+++ src/components/PortfolioSectionCompopnent/PortfolioSectionCompopnent.tsx
@@ -1,7 +1,7 @@
 import PortfolioComponent from "./PortfolioComponent";
 import s from "./PortfolioSectionCompopnent.module.css";
 import portfolioDB from "../../assets/data/portfolioDB";
-import { Swiper, SwiperSlide, type SwiperRef } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
@@ -12,69 +12,71 @@ import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import portfolioAnimation from "../animation/portfolioAnimation";
 
-
-
 const PortfolioSectionCompopnent: React.FC = () => {
-   const paginationPortfolioRef = useRef<HTMLDivElement | null>(null);
-   const {t} = useTranslation()
-const portfolioTrigger = useRef<HTMLDivElement | null>(null);
+  const paginationPortfolioRef = useRef<HTMLDivElement | null>(null);
+  const { t } = useTranslation();
+  const portfolioTrigger = useRef<HTMLDivElement | null>(null);
   const portfolioTitle = useRef<HTMLDivElement | null>(null);
   const portfolioList = useRef<HTMLDivElement | null>(null);
-useGSAP(()=>{
-if (!portfolioTitle.current || !portfolioList.current ) return;
+  useGSAP(() => {
+    if (!portfolioTitle.current || !portfolioList.current) return;
 
-portfolioAnimation(portfolioTitle.current,portfolioList.current,portfolioTrigger.current as HTMLElement )
-})
+    portfolioAnimation(
+      portfolioTitle.current,
+      portfolioList.current,
+      portfolioTrigger.current as HTMLElement
+    );
+  });
 
   return (
-    <div ref={portfolioTrigger} className={s.portfolioSection} >
+    <div ref={portfolioTrigger} className={s.portfolioSection}>
       <div className="container" id="ourWorks">
-        <h2 ref={portfolioTitle} className={s.portfolioTitle}>{t("Portfolio.portfolio")}</h2>
+        <h2 ref={portfolioTitle} className={s.portfolioTitle}>
+          {t("Portfolio.portfolio")}
+        </h2>
       </div>
-      <div   ref={portfolioList}> 
-      <Swiper
-     
-        className={s.portfolioSlider}
-        modules={[EffectCoverflow, Pagination, Keyboard]}
-        spaceBetween={35}
-        centeredSlides={true}
-        effect="coverflow"
-        grabCursor={true}
-        loop={true}
-        slidesPerView="auto"
-        watchSlidesProgress={true}
-         keyboard={{
-          enabled: true,
-        }}
+      <div ref={portfolioList}>
+        <Swiper
+          className={s.portfolioSlider}
+          modules={[EffectCoverflow, Pagination, Keyboard]}
+          spaceBetween={35}
+          centeredSlides={true}
+          effect="coverflow"
+          grabCursor={true}
+          loop={true}
+          slidesPerView="auto"
+          watchSlidesProgress={true}
+          keyboard={{
+            enabled: true,
+          }}
           pagination={{
-          el: `.${s.customPaginationProjects}`,
-          clickable: true,
-        }}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        
-        
-      >
-        {portfolioDB.map((item, idx) => (
-          <SwiperSlide key={idx} className={s.slide}>
-          
+            el: `.${s.customPaginationProjects}`,
+            clickable: true,
+          }}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+        >
+          {portfolioDB.map((item, idx) => (
+            <SwiperSlide key={idx} className={s.slide}>
               <PortfolioComponent webPage={item} />
-          
-          </SwiperSlide>
-        ))}
-        <div className={s.navWrapper}>
-  <SwiperNavigationComponent type="prev" />
+            </SwiperSlide>
+          ))}
+          <div className={s.navWrapper}>
+            <SwiperNavigationComponent type="prev" />
 
-  <div ref={paginationPortfolioRef} className={s.customPaginationProjects}></div>
+            <div
+              ref={paginationPortfolioRef}
+              className={s.customPaginationProjects}
+            ></div>
 
-  <SwiperNavigationComponent type="next" />
-</div>
-      </Swiper>
+            <SwiperNavigationComponent type="next" />
+          </div>
+        </Swiper>
       </div>
     </div>
   );
