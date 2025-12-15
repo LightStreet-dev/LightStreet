d591c112a67551ad63dd42de39eacc85f535dc32
LightStreet <light.street.web@gmail.com>
2025-12-15 21:56:33 +0100

    benefits animation end

--- src/components/animation/benefitsAnimation.ts
+++ src/components/animation/benefitsAnimation.ts
@@ -38,21 +38,92 @@ const benefotsAnimation = (
       opacity: 0,
       duration: 0.5,
     });
-    tl.from(benefitsSVG2, {
+    tl.from(
+      benefitsSVG2,
+      {
+        x: -50,
+        opacity: 0,
+        duration: 0.5,
+      },
+      "-=0.5"
+    );
+    tl.from(
+      benefitsSVG3,
+      {
+        x: -50,
+        opacity: 0,
+        duration: 0.5,
+      },
+      "-=0.5"
+    );
+    tl.from(
+      benefitsSVG4,
+      {
+        x: -50,
+        opacity: 0,
+        duration: 0.5,
+      },
+      "-=0.5"
+    );
+  });
+  media.add("(min-width: 769px)", () => {
+    const tl = gsap.timeline({
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: benefitsTrigger,
+        start: "top 70%",
+        toggleActions: "play none none none",
+        markers: true,
+      },
+    });
+
+    tl.from(benefitsTitle, {
+      y: 50,
+      opacity: 0,
+      duration: 0.5,
+    });
+    tl.from(benefitsList, {
+      y: 50,
+      opacity: 0,
+      duration: 0.5,
+       "--benefitsAnimationLineStart": "-200%",
+    "--benefitsAnimationLineEnd": "200%",
+    });
+
+
+
+    tl.from(benefitsSVG1, {
       x: -50,
       opacity: 0,
       duration: 0.5,
-    },"-=0.5");
-    tl.from(benefitsSVG3, {
-      x: -50,
-      opacity: 0,
-      duration: 0.5,
-    },"-=0.5");
-    tl.from(benefitsSVG4, {
-      x: -50,
-      opacity: 0,
-      duration: 0.5,
-    },"-=0.5");
+    });
+    tl.from(
+      benefitsSVG2,
+      {
+        x: -50,
+        opacity: 0,
+        duration: 0.5,
+      },
+      "-=0.5"
+    );
+    tl.from(
+      benefitsSVG3,
+      {
+        x: -50,
+        opacity: 0,
+        duration: 0.5,
+      },
+      "-=0.5"
+    );
+    tl.from(
+      benefitsSVG4,
+      {
+        x: -50,
+        opacity: 0,
+        duration: 0.5,
+      },
+      "-=0.5"
+    );
   });
 };
 export default benefotsAnimation;
