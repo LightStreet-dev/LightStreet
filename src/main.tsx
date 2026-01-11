88a816b166d523a491dc7b88ac7900f54e6a666c
LightStreet <light.street.web@gmail.com>
2026-01-11 13:01:27 +0100

    if in main

--- src/main.tsx
+++ src/main.tsx
@@ -8,21 +8,14 @@ import { PropagateLoader } from 'react-spinners'
 import { i18nReady } from './i18n' // твій проміс i18nReady
 
 const root = createRoot(document.getElementById('root')!)
-
-i18nReady.then(() => {
-  root.render(
-    <StrictMode>
-      <Suspense
-        fallback={
-          <PropagateLoader
-            className="loader LSloader"
-            color="hsla(204, 100%, 50%, 0.08)"
-            size={30}
-          />
-        }
-      >
-        <App />
-      </Suspense>
-    </StrictMode>
-  )
-})
+if (i18nReady) {
+  i18nReady.then(() => {
+    root.render(
+      <StrictMode>
+        <Suspense fallback={<PropagateLoader />}>
+          <App />
+        </Suspense>
+      </StrictMode>
+    )
+  })
+}
